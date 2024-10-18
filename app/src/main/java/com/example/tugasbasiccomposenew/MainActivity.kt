adae01450cb19852d5534fa060ef96b7fc6eac22
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2024-10-19 01:16:06 +0700

    Mengaktifkan fitur edge-to-edge pada tampilan

--- app/src/main/java/com/example/tugasbasiccomposenew/MainActivity.kt
+++ app/src/main/java/com/example/tugasbasiccomposenew/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
+
         setContent {
             TugasBasicComposeNewTheme {
 
