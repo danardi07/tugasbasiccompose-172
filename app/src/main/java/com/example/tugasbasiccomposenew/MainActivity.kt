6b9c7d28a885a18b81759a03497486ce649759ae
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2024-10-19 01:14:56 +0700

    Membuat kelas MainActivity untuk aplikasi

--- app/src/main/java/com/example/tugasbasiccomposenew/MainActivity.kt
+++ app/src/main/java/com/example/tugasbasiccomposenew/MainActivity.kt
@@ -29,6 +29,7 @@ import com.example.tugasbasiccomposenew.ui.theme.TugasBasicComposeNewTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
             TugasBasicComposeNewTheme {
